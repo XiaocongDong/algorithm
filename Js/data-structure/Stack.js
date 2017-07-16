fbdc7a76c5fcefd009d3795826290f2f935e5621
Xiaocong Dong <15652914852@163.com>
2017-07-15 21:09:05 -0700

    added stack data structure

---
+++ Js/data-structure/Stack.js
@@ -0,0 +1,18 @@
+class Stack {
+
+    constructor() {
+        this.stack = new Array();
+    }
+
+    push(d) {
+        this.stack.push(d);
+    }
+
+    pop() {
+        return this.stack.pop();
+    }
+
+    isEmpty() {
+        return this.stack.length === 0;
+    }
+}
